e762c56d7f3b55b36f0a56be3398bf43a5bda0b1
Sergio_Mir <voidtracer6@gmail.com>
2024-03-30 01:21:25 -0400

    mod

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -19,7 +18,5 @@ const router = createHashRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
 )

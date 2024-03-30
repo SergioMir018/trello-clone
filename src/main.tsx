b5f1e756c37c482536f6f0e11cd61fb779093a7c
Sergio_Mir <voidtracer6@gmail.com>
2024-03-30 11:04:34 -0400

    mod

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Board from './components/Board.tsx'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
       {
-        path: '/board/:boardId',
+        path: 'board/:boardId',
         element: <Board />
       }
     ]
